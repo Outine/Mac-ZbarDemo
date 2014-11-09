a5631be1e7aaa052cfc1df00a18e3ee190c84865
James Hutton <jh849@cam.ac.uk>
2014-11-09 14:52:19 +0000

    tidy

--- zbarDemo.py
+++ zbarDemo.py
@@ -20,14 +20,15 @@ scanner.scan(image)
 draw = ImageDraw.Draw(pil)
 for symbol in image:
     # So we get some numbers out, not really sure what they are. Might as well draw some lines and see what happens.
+    #I think the longest line should be our "best" match... but why can't it give us a bounding box or something! Nuts.
     print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
     # clean up
     pairs = len(symbol.location)
-    for x in range(3):
+    for x in range(pairs):
       print symbol.location[x], symbol.location[x-1]
       draw.line([symbol.location[x], symbol.location[x-1]])
 
 pil.show()
-pil.save("MarkedUpBarcode","PNG")
+pil.save("MarkedUpBarcode.png","PNG")
 del(image)
 del(draw)
